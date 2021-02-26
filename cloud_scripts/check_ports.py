e401112af77a00109cf7fb3b57b2e317cc780f90
Vlad Zolotarov <vladz@scylladb.com>
2021-02-25 20:22:20 -0500

    check_ports.py: basic ports scanner
    Checks TCP connectivity from the local node to
    given addresses on given TCP ports.
    
    In case of a successful scan exits with a 0 exit status.
    
    Successful scan example:
    
    $ ./check_ports.py --remote-address 127.0.0.1 --remote-address 127.0.0.2 --port 7000 --port 10000
    Checking address 127.0.0.2
      Checking port 10000
      127.0.0.2:10000 is ok
      Checking port 7000
      127.0.0.2:7000 is ok
    Checking address 127.0.0.1
      Checking port 10000
      127.0.0.1:10000 is ok
      Checking port 7000
      127.0.0.1:7000 is ok
    Checks passed
    
    Unsuccessful scan example:
    
    $ ./check_ports.py --remote-address 127.0.0.1 --remote-address 127.0.0.10 --port 7000 --port 10000
    Checking address 127.0.0.1
      Checking port 10000
      127.0.0.1:10000 is ok
      Checking port 7000
      127.0.0.1:7000 is ok
    Checking address 127.0.0.10
      Checking port 10000
      127.0.0.10:10000 is not open: (<class 'ConnectionRefusedError'>,
    ConnectionRefusedError(111, 'Connection refused'), <traceback object at
    0x7feb839a8440>)
      Checking port 7000
      127.0.0.10:7000 is not open: (<class 'ConnectionRefusedError'>,
    ConnectionRefusedError(111, 'Connection refused'), <traceback object at
    0x7feb83928a40>)
    One or more ports were not open
    
    Signed-off-by: Vlad Zolotarov <vladz@scylladb.com>

---
+++ cloud_scripts/check_ports.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+import argparse
+import socket
+import sys
+
+################################################################################
+argp = argparse.ArgumentParser(description = "Check that given ports are open on remote addresses.", formatter_class=argparse.RawDescriptionHelpFormatter,
+                               epilog=
+'''
+Tries to connect to the given set of TCP ports on given addresses set.
+''')
+argp.add_argument('--remote-address', help='remote addresses list', default=[], action='append', dest='remote_address_set')
+argp.add_argument('--port', help='Remote ports to check', default=[], action='append', dest='ports_set')
+
+args = argp.parse_args()
+
+# Remote duplicates
+remote_address_set = set(args.remote_address_set)
+ports_set = set(args.ports_set)
+checked = False
+success = True
+
+for addr in remote_address_set:
+    print("Checking address {}".format(addr))
+
+    for port in ports_set:
+        checked = True
+
+        print("  Checking port {}".format(port))
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((addr, int(port)))
+            print("  {}:{} is ok".format(addr, port))
+            s.shutdown(socket.SHUT_RDWR)
+            s.close()
+        except:
+            print("  {}:{} is not open: {}".format(addr, port, sys.exc_info()))
+            success = False
+
+if checked:
+    if success:
+        print("Checks passed")
+        sys.exit(0)
+    else:
+        print("One or more ports were not open")
+        sys.exit(1)
+else:
+    print("Nothing checked")
+    sys.exit(1)
+
